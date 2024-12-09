a25ec34d89843e6da0b4d561417f9820aff76bbc
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-12-10 04:59:40 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py 
-api_key = “AIzaSyCsfIKXHr0AY8RneP2VWpjdR038aMNkCsw” 
-
-# app.py 
-import config
-city_id = “5128581” 
-base_url = "https://api.openweathermap.org/data/2.5/weather?" 
-final_url = base_url + "appid=" + config.api_key + "&id=" + city_id

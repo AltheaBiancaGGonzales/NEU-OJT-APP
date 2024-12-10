267618f96f2623f19f961a251f587d9113525759
teachemeeeeeeeeeeee <135201717+teachemeeeeeeeeeeee@users.noreply.github.com>
2024-12-10 20:20:51 +0800

    Update register.js

--- register.js
+++ register.js
@@ -1,22 +1,12 @@
- 
- API_KEY = "AIzaSyCsfIKXHr0AY8RneP2VWpjdR038aMNkCsw"
-
- 
- 
- // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCsfIKXHr0AY8RneP2VWpjdR038aMNkCsw",
-    authDomain: "out-of-bounds-ojt-app.firebaseapp.com",
-    projectId: "out-of-bounds-ojt-app",
-    storageBucket: "out-of-bounds-ojt-app.firebasestorage.app",
-    messagingSenderId: "1059978490785",
-    appId: "1:1059978490785:web:d08f9e40b5b0e58efed1e0"
+    apiKey: "AIzaSyD2F2qw1S4sa6K0BjG9XEf72x5h-gY8JV0",
+    authDomain: "neu-ojt-app-bd4c6.firebaseapp.com",
+    projectId: "neu-ojt-app-bd4c6",
+    storageBucket: "neu-ojt-app-bd4c6.firebasestorage.app",
+    messagingSenderId: "175965892446",
+    appId: "1:175965892446:web:6062d7e0739ffa4a7369cf"
   };
 
-  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

97d4d4b7fcc9fd08adbf3f8a746dd5b2d8f80a15
teachemeeeeeeeeeeee <135201717+teachemeeeeeeeeeeee@users.noreply.github.com>
2024-12-10 21:40:44 +0800

    Update register.js

--- register.js
+++ register.js
@@ -14,7 +14,7 @@
   const auth = getAuth(app);
   auth.languageCode = 'en'
   const  provider  = new GoogleAuthProvider();
-  const googleLogin = document.getElementId("google-signin");
+  const googleLogin = document.getElementId("google-login-btn");
   googleLogin.addEventListener("click", function(){
   signInWithPopup(auth, provider)
   .then((result) => {
